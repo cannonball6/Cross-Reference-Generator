f2aab03263e337fd0302c5272c8f246e3831aeb2
cannonball6 <lindley.cannon@gmail.com>
2015-04-15 18:47:44 -0700

    Update

--- DS Final/main.c
+++ DS Final/main.c
@@ -1,15 +1,51 @@
-//
-//  main.c
-//  DS Final
-//
-//  Created by Lindley P Cannon Jr on 4/15/15.
-//  Copyright (c) 2015 LindleyP. All rights reserved.
-//
+
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here... ok
-    printf("Hello, World!\n");
+#include <stdlib.h>
+int main (void)
+{
+    //    int c;
+    //    FILE *fptr;
+    char *mode = "r+";
+    
+    //
+    //    fptr = fopen("/Users/Cannon/Documents/Newbee.c", mode);
+    
+    
+    
+    // the file we want to read from
+    char filename[] = "/Users/Cannon/Documents/Newbee.c";
+    
+    // open the file for reading
+    FILE *file = fopen("/Users/Cannon/Documents/Newbee.c", mode);
+    FILE *out = fopen("/Users/Cannon/Documents/NewbOut.c", "w");
+    
+    // make sure the file opened properly
+    if(NULL == file)
+    {
+        fprintf(stderr, "Cannot open file: %s\n", filename);
+        return 1;
+    }
+    
+    size_t buffer_size = 80;
+    char *buffer = malloc(buffer_size * sizeof(char));
+    
+    // read each line and print it to the screen
+    int line_number = 0;
+    while(-1 != getline(&buffer, &buffer_size, file))
+    {
+        //printf("%d: %s", ++line_number, buffer);
+        fprintf(out, "%d: %s", ++line_number, buffer);
+    }
+    fflush(stdout);
+    
+    // make sure we close the file and clean up the buffer when we're
+    // finished
+    fclose(file);
+    free(buffer);
+    
+    
+    
+    
     return 0;
 }
