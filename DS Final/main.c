08626cf5ddff7f744aa90c263b7409d6a7337d94
cannonball6 <lindley.cannon@gmail.com>
2015-04-15 18:40:34 -0700

    First Commit

---
+++ DS Final/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  DS Final
+//
+//  Created by Lindley P Cannon Jr on 4/15/15.
+//  Copyright (c) 2015 LindleyP. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here... ok
+    printf("Hello, World!\n");
+    return 0;
+}
